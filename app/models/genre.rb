0795c1c6caefcdff924b5324d6edc2736d3b244d
Ned-el-ch <nedelchev.niki@gmail.com>
2019-10-22 08:50:57 +0100

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,16 +1,18 @@
 class Genre < ActiveRecord::Base
-  has_many :songs
-  has_many :artists, through: :songs
+    has_many :songs
+    has_many :artists, through: :songs
 
-  def song_count
-    # return the number of songs in a genre
-  end
+    def song_count
 
-  def artist_count
-    # return the number of artists associated with the genre
-  end
+        self.songs.size
+        
+    end
 
-  def all_artist_names
-    # return an array of strings containing every musician's name
-  end
+    def artist_count
+        self.artists.size
+    end
+
+    def all_artist_names
+        self.artists.map {|artist| artist.name}
+    end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,30 @@
+require 'pry'
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
-  def get_genre_name
-  end
+    def get_genre_name
 
-  def drake_made_this
-    # when this method is called it should assign the song's artist to Drake
-    # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
-    # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
+        self.genre.name
+        
+    end
 
-  end
+    def drake_made_this
+        # binding.pry
+        drake_is_real = Artist.all.find {|artist| artist.name == "Drake"}
+        # binding.pry
+        if !drake_is_real
+
+            drake_is_real = Artist.create(name: "Drake")
+            
+        end
+        # binding.pry
+        drake_is_real.songs << self
+
+        # when this method is called it should assign the song's artist to Drake
+        # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
+        # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
+
+    end
 end
